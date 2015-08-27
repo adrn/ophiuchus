4dbce40c05156fcd12d102090cd8adcd75c858fd
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-08-27 16:29:55 -0400

    dummy test to try import

---
+++ ophiuchus/tests/test_core.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+""" Test ...  """
+
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os
+import sys
+import logging
+
+# Third-party
+from astropy import log as logger
+import matplotlib.pyplot as pl
+import numpy as np
+
+# Project
+from ..potential import WangZhaoBarPotential
+
+def test_derp():
+    pass
