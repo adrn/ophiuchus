9dec83221a6c3fbd9f7d3ba9ac132ebea23e4651
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-10-03 23:37:32 -0400

    switch to yaml loading potentials

--- ophiuchus/__init__.py
+++ ophiuchus/__init__.py
@@ -12,12 +12,4 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from .potential import OphiuchusPotential
-
-    # These are the canonical potentials I'll use in the paper:
-    #   - One Milky Way model with a time-dependent Bar
-    #   - One axisymmetric, static Milky Way model
-    barred_mw = OphiuchusPotential()
-    static_mw = OphiuchusPotential(bar=dict(m=0.),
-                                   spheroid=dict(m=1E10, c=0.2),
-                                   disk=dict(m=6.5E10))
+    pass
