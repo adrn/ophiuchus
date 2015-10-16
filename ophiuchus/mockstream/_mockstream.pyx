d52b80fb4272acbc281524074e63a56ff1a4b45a
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-10-16 14:11:08 -0400

    added fardal stream generator too

--- ophiuchus/mockstream/_mockstream.pyx
+++ ophiuchus/mockstream/_mockstream.pyx
@@ -49,12 +49,13 @@ cdef extern from "stdio.h":
     ctypedef struct FILE
     FILE *stdout
 
-cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] prog_w,
-                        int release_every,
-                        double G, double[::1] prog_mass,
-                        double atol=1E-10, double rtol=1E-10, int nmax=0):
+cpdef mock_stream(_CPotential cpotential, double[::1] t, double[:,::1] prog_w,
+                  int release_every,
+                  double[::1] k_mean, double[::1] k_disp,
+                  double G, double[::1] prog_mass,
+                  double atol=1E-10, double rtol=1E-10, int nmax=0):
     """
-    streakline_stream(cpotential, t, prog_w, release_every, G, prog_mass, atol, rtol, nmax)
+    mock_stream(cpotential, t, prog_w, release_every, k_mean, k_disp, G, prog_mass, atol, rtol, nmax)
 
     Generate a mock stellar stream using the Streakline method.
 
@@ -69,6 +70,14 @@ cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] pro
         Should have shape ``(ntimesteps,6)``.
     release_every : int
         Release particles at the Lagrange points every X timesteps.
+    k_mean : `numpy.ndarray`
+        Array of mean ``k`` values (see Fardal et al. 2015). These are used to determine
+        the exact prescription for generating the mock stream. The components are for:
+        ``(R,phi,z,vR,vphi,vz)``.
+    k_disp : `numpy.ndarray`
+        Array of ``k`` value dispersions (see Fardal et al. 2015). These are used to determine
+        the exact prescription for generating the mock stream. The components are for:
+        ``(R,phi,z,vR,vphi,vz)``.
     G : numeric
         The value of the gravitational constant, G, in the unit system used.
     prog_mass : `numpy.ndarray`
@@ -97,6 +106,9 @@ cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] pro
         double[::1] cyl = np.zeros(6) # 6-position in cylindrical coords
         double[::1] prog_w_prime = np.zeros(6) # 6-position of progenitor rotated
         double[::1] prog_cyl = np.zeros(6) # 6-position of progenitor in cylindrical coords
+
+        # k-factors for parametrized model of Fardal et al. (2015)
+        double[::1] ks = np.zeros(6)
 
         # used for figuring out how many orbits to integrate at any given release time
         unsigned this_ndim, this_norbits
@@ -131,9 +143,8 @@ cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] pro
 
         i += 1
 
-    # now go back to each set of initial conditions and:
-    #   - put position at +/- tidal radius along radial vector
-    #   - set velocity so angular velocity constant
+    # now go back to each set of initial conditions and modify initial condition
+    #   based on mock prescription
     i = 0
     for j in range(nsteps):
         if (j % release_every) != 0:
@@ -143,34 +154,39 @@ cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] pro
         d = sqrt(prog_w[j,0]*prog_w[j,0] +
                  prog_w[j,1]*prog_w[j,1] +
                  prog_w[j,2]*prog_w[j,2])
-        Om = np.linalg.norm(np.cross(prog_w[j,:3], prog_w[j,3:]) / d)
+        Om = np.linalg.norm(np.cross(prog_w[j,:3], prog_w[j,3:]) / d**2)
 
         # gradient of potential in radial direction
-        menc = cpotential._mass_enclosed(t[j], &prog_w[j,0], &eps[0], G)
-        f = 1. + cpotential._d2_dr2(t[j], &prog_w[j,0], &eps[0], G) / (Om*Om)
-        r_tide = (G*prog_mass[j] / (f*menc))**(1/3.)
+        f = Om*Om - cpotential._d2_dr2(t[j], &prog_w[j,0], &eps[0], G)
+        r_tide = (G*prog_mass[j] / f)**(1/3.)
 
         # the rotation matrix to transform from satellite coords to normal
         sat_rotation_matrix(&prog_w[j,0], &R[0,0])
         to_sat_coords(&prog_w[j,0], &R[0,0], &prog_w_prime[0])
         car_to_cyl(&prog_w_prime[0], &prog_cyl[0])
 
+        for k in range(6):
+            if k_disp[k] > 0:
+                ks[k] = np.random.normal(k_mean[k], k_disp[k])
+            else:
+                ks[k] = k_mean[k]
+
         # eject stars at tidal radius with same angular velocity as progenitor
-        cyl[0] = prog_cyl[0] + r_tide
-        cyl[1] = 0.
-        cyl[2] = 0.
-        cyl[3] = prog_cyl[3]
-        cyl[4] = prog_cyl[4] * cyl[0] / prog_cyl[0]
-        cyl[5] = prog_cyl[5]
+        cyl[0] = prog_cyl[0] + ks[0]*r_tide
+        cyl[1] = prog_cyl[1] + ks[1]*r_tide/prog_cyl[0]
+        cyl[2] = ks[2]*r_tide/prog_cyl[0]
+        cyl[3] = prog_cyl[3] + ks[3]*prog_cyl[3]
+        cyl[4] = prog_cyl[4] + ks[0]*ks[4]*Om*r_tide
+        cyl[5] = ks[5]*Om*r_tide
         cyl_to_car(&cyl[0], &w_prime[0])
         from_sat_coords(&w_prime[0], &R[0,0], &w[2*i*ndim])
 
-        cyl[0] = prog_cyl[0] - r_tide
-        cyl[1] = 0.
-        cyl[2] = 0.
-        cyl[3] = prog_cyl[3]
-        cyl[4] = prog_cyl[4] * cyl[0] / prog_cyl[0]
-        cyl[5] = prog_cyl[5]
+        cyl[0] = prog_cyl[0] - ks[0]*r_tide
+        cyl[1] = prog_cyl[1] - ks[1]*r_tide/prog_cyl[0]
+        cyl[2] = ks[2]*r_tide/prog_cyl[0]
+        cyl[3] = prog_cyl[3] + ks[3]*prog_cyl[3]
+        cyl[4] = prog_cyl[4] - ks[0]*ks[4]*Om*r_tide
+        cyl[5] = ks[5]*Om*r_tide
         cyl_to_car(&cyl[0], &w_prime[0])
         from_sat_coords(&w_prime[0], &R[0,0], &w[2*i*ndim + ndim])
 
@@ -199,164 +215,120 @@ cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] pro
 
     return np.asarray(w).reshape(nparticles, ndim)
 
-# cpdef fardal_stream(_CPotential cpotential, double[::1] t, double[:,::1] prog_w,
-#                     int release_every,
-#                     double G, double[::1] prog_mass,
-#                     double atol=1E-10, double rtol=1E-10, int nmax=0):
-#     """
-#     fardal_stream(cpotential, t, prog_w, release_every, G, prog_mass, atol, rtol, nmax)
+cpdef streakline_stream(_CPotential cpotential, double[::1] t, double[:,::1] prog_w,
+                        int release_every,
+                        double G, double[::1] prog_mass,
+                        double atol=1E-10, double rtol=1E-10, int nmax=0):
+    """
+    streakline_stream(cpotential, t, prog_w, release_every, G, prog_mass, atol, rtol, nmax)
 
-#     Generate a mock stellar stream using the particle-spray method
-#     from Fardal et al. (2015).
+    Generate a mock stellar stream using the Streakline method.
 
-#     Parameters
-#     ----------
-#     cpotential : `gary.potential._CPotential`
-#         An instance of a ``_CPotential`` representing the gravitational potential.
-#     t : `numpy.ndarray`
-#         An array of times. Should have shape ``(ntimesteps,)``.
-#     prog_w : `numpy.ndarray`
-#         The 6D coordinates for the orbit of the progenitor system at all times.
-#         Should have shape ``(ntimesteps,6)``.
-#     release_every : int
-#         Release particles at the Lagrange points every X timesteps.
-#     G : numeric
-#         The value of the gravitational constant, G, in the unit system used.
-#     prog_mass : `numpy.ndarray`
-#         The mass of the progenitor at each time. Should have shape ``(ntimesteps,)``.
-#     atol : numeric (optional)
-#         Passed to the integrator. Absolute tolerance parameter. Default is 1E-10.
-#     rtol : numeric (optional)
-#         Passed to the integrator. Relative tolerance parameter. Default is 1E-10.
-#     nmax : int (optional)
-#         Passed to the integrator.
-#     """
-#     cdef:
-#         int i, j, k # indexing
-#         int res # result from calling dop853
-#         int ntimes = t.shape[0] # number of times
-#         int nsteps = ntimes-1 # number of steps
-#         int nparticles # total number of test particles released
+    Parameters
+    ----------
+    cpotential : `gary.potential._CPotential`
+        An instance of a ``_CPotential`` representing the gravitational potential.
+    t : `numpy.ndarray`
+        An array of times. Should have shape ``(ntimesteps,)``.
+    prog_w : `numpy.ndarray`
+        The 6D coordinates for the orbit of the progenitor system at all times.
+        Should have shape ``(ntimesteps,6)``.
+    release_every : int
+        Release particles at the Lagrange points every X timesteps.
+    G : numeric
+        The value of the gravitational constant, G, in the unit system used.
+    prog_mass : `numpy.ndarray`
+        The mass of the progenitor at each time. Should have shape ``(ntimesteps,)``.
+    atol : numeric (optional)
+        Passed to the integrator. Absolute tolerance parameter. Default is 1E-10.
+    rtol : numeric (optional)
+        Passed to the integrator. Relative tolerance parameter. Default is 1E-10.
+    nmax : int (optional)
+        Passed to the integrator.
+    """
+    cdef:
+        double[::1] k_mean = np.zeros(6)
+        double[::1] k_disp = np.zeros(6)
 
-#         unsigned ndim = prog_w.shape[1] # phase-space dimensionality
-#         unsigned ndim_2 = ndim / 2 # configuration-space dimensionality
+    k_mean[0] = 1. # R
+    k_disp[0] = 0.
 
-#         double dt0 = t[1] - t[0] # initial timestep
-#         double[::1] tmp = np.zeros(3) # temporary array
+    k_mean[1] = 0. # phi
+    k_disp[1] = 0.
 
-#         # used for figuring out how many orbits to integrate at any given release time
-#         unsigned this_ndim, this_norbits
+    k_mean[2] = 0. # z
+    k_disp[2] = 0.
 
-#         double Om2 # angular velocity squared
-#         double d, sigma_r # distance, dispersion in release positions
-#         double r_tide, menc, f # tidal radius, mass enclosed, f factor
+    k_mean[3] = 0. # vR
+    k_disp[3] = 0.
 
-#         double[::1] eps = np.zeros(3) # used for 2nd derivative estimation
+    k_mean[4] = 1. # vt
+    k_disp[4] = 0.
 
-#     # figure out how many particles are going to be released into the "stream"
-#     if nsteps % release_every == 0:
-#         nparticles = 2 * (nsteps // release_every)
-#     else:
-#         nparticles = 2 * (nsteps // release_every + 1)
+    k_mean[5] = 0. # vz
+    k_disp[5] = 0.
 
-#     # container for only current positions of all particles
-#     cdef double[::1] w = np.empty(nparticles*ndim)
+    return mock_stream(cpotential, t, prog_w,
+                       release_every,
+                       k_mean, k_disp,
+                       G, prog_mass,
+                       atol=atol, rtol=rtol, nmax=nmax)
 
-#     # TODO: this should be a class...
-#     # ------- Parameters  -------
-#     cdef:
-#         double kr_mean = 2.
-#         double kr_disp = 0.5
-#         double kphi = 0.
-#         double kz_mean = 0.
-#         double kz_disp = 0.5
+cpdef fardal_stream(_CPotential cpotential, double[::1] t, double[:,::1] prog_w,
+                    int release_every,
+                    double G, double[::1] prog_mass,
+                    double atol=1E-10, double rtol=1E-10, int nmax=0):
+    """
+    fardal_stream(cpotential, t, prog_w, release_every, G, prog_mass, atol, rtol, nmax)
 
-#         double kvr = 0.
-#         double kvt_mean = 0.3
-#         double kvt_disp = 0.5
-#         double kvz_mean = 0.
-#         double kvz_disp = 0.5
+    Generate a mock stellar stream using the Streakline method.
 
-#     # copy over initial conditions from progenitor orbit to each streakline star
-#     i = 0
-#     for j in range(nsteps):
-#         if (j % release_every) != 0:
-#             continue
+    Parameters
+    ----------
+    cpotential : `gary.potential._CPotential`
+        An instance of a ``_CPotential`` representing the gravitational potential.
+    t : `numpy.ndarray`
+        An array of times. Should have shape ``(ntimesteps,)``.
+    prog_w : `numpy.ndarray`
+        The 6D coordinates for the orbit of the progenitor system at all times.
+        Should have shape ``(ntimesteps,6)``.
+    release_every : int
+        Release particles at the Lagrange points every X timesteps.
+    G : numeric
+        The value of the gravitational constant, G, in the unit system used.
+    prog_mass : `numpy.ndarray`
+        The mass of the progenitor at each time. Should have shape ``(ntimesteps,)``.
+    atol : numeric (optional)
+        Passed to the integrator. Absolute tolerance parameter. Default is 1E-10.
+    rtol : numeric (optional)
+        Passed to the integrator. Relative tolerance parameter. Default is 1E-10.
+    nmax : int (optional)
+        Passed to the integrator.
+    """
+    cdef:
+        double[::1] k_mean = np.zeros(6)
+        double[::1] k_disp = np.zeros(6)
 
-#         for k in range(ndim):
-#             w[2*i*ndim + k] = prog_w[j,k]
-#             w[2*i*ndim + k + ndim] = prog_w[j,k]
+    k_mean[0] = 2. # R
+    k_disp[0] = 0.5
 
-#         i += 1
+    k_mean[1] = 0. # phi
+    k_disp[1] = 0.
 
-#     # now go back to each set of initial conditions and:
-#     #   - put position at +/- tidal radius along radial vector
-#     #   - set velocity so angular velocity constant
-#     #   - add dispersion if specified
-#     i = 0
-#     for j in range(nsteps):
-#         if (j % release_every) != 0:
-#             continue
+    k_mean[2] = 0. # z
+    k_disp[2] = 0.5
 
-#         # get angular velocity of the progenitor
-#         v_car_to_cyl(&w[2*i*ndim], &w[2*i*ndim + 3], &tmp[0])
-#         Om2 = tmp[1]*tmp[1] + tmp[2]*tmp[2]
+    k_mean[3] = 0. # vR
+    k_disp[3] = 0.
 
-#         # gradient of potential in radial direction
-#         menc = cpotential._mass_enclosed(t[j], &prog_w[j,0], &eps[0], G)
-#         f = 1. + cpotential._d2_dr2(t[j], &prog_w[j,0], &eps[0], G) / Om2
-#         r_tide = (G*prog_mass[j] / (f*menc))**(1/3.)
+    k_mean[4] = 0.3 # vt
+    k_disp[4] = 0.5
 
-#         # -------- Position --------
-#         car_to_sph(&w[2*i*ndim], &tmp[0])
-#         d = tmp[0]
-#         tmp[0] = d + r_tide*np.random.normal(kr_mean, kr_disp)
-#         sph_to_car(&tmp[0], &w[2*i*ndim])
+    k_mean[5] = 0. # vz
+    k_disp[5] = 0.5
 
-#         car_to_sph(&w[2*i*ndim + ndim], &tmp[0])
-#         tmp[0] = d - r_tide*np.random.normal(kr_mean, kr_disp)
-#         sph_to_car(&tmp[0], &w[2*i*ndim + ndim])
-
-#         # sigmar = pos_disp_fac * r_tide / 1.732 # sqrt(3)
-#         # if sigmar > 0:
-#         #     for k in range(ndim_2):
-#         #         w[2*i*ndim + k] = np.random.normal(w[2*i*ndim + k], sigmar)
-#         #         w[2*i*ndim + k + ndim] = np.random.normal(w[2*i*ndim + k + ndim], sigmar)
-
-#         # -------- Velocity --------
-#         v_car_to_sph(&w[2*i*ndim], &w[2*i*ndim + ndim_2], &tmp[0])
-#         # tmp[0] = np.random.normal(tmp[0], vel_disp) # radial velocity dispersion
-#         tmp[1] = tmp[1] * (d+r_tide) / d
-#         tmp[2] = tmp[2] * (d+r_tide) / d
-#         v_sph_to_car(&w[2*i*ndim], &tmp[0], &w[2*i*ndim + ndim_2])
-
-#         v_car_to_sph(&w[2*i*ndim + ndim], &w[2*i*ndim + ndim + ndim_2], &tmp[0])
-#         # tmp[0] = np.random.normal(tmp[0], vel_disp) # radial velocity dispersion
-#         tmp[1] = tmp[1] * (d-r_tide) / d
-#         tmp[2] = tmp[2] * (d-r_tide) / d
-#         v_sph_to_car(&w[2*i*ndim + ndim], &tmp[0], &w[2*i*ndim + ndim + ndim_2])
-
-#         i += 1
-
-#     i = 1
-#     for j in range(nsteps):
-#         if j % release_every == 0:
-#             this_norbits = 2*i
-#             this_ndim = ndim * this_norbits
-#             i += 1
-
-#         res = dop853(this_ndim, <FcnEqDiff> Fwrapper,
-#                      <GradFn>cpotential.c_gradient, &(cpotential._parameters[0]), this_norbits,
-#                      t[j], &w[0], t[j+1], &rtol, &atol, 0, NULL, 0,
-#                      NULL, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, dt0, nmax, 0, 1, 0, NULL, 0);
-
-#         if res == -1:
-#             raise RuntimeError("Input is not consistent.")
-#         elif res == -2:
-#             raise RuntimeError("Larger nmax is needed.")
-#         elif res == -3:
-#             raise RuntimeError("Step size becomes too small.")
-#         elif res == -4:
-#             raise RuntimeError("The problem is probably stff (interrupted).")
-
-#     return np.asarray(w).reshape(nparticles, ndim)
+    return mock_stream(cpotential, t, prog_w,
+                       release_every,
+                       k_mean, k_disp,
+                       G, prog_mass,
+                       atol=atol, rtol=rtol, nmax=nmax)
--- ophiuchus/mockstream/tests/test_mockstream.py
+++ ophiuchus/mockstream/tests/test_mockstream.py
@@ -15,26 +15,32 @@ import gary.potential as gp
 from gary.units import galactic
 
 # Project
-from .._mockstream import streakline_stream
+from .._mockstream import streakline_stream, fardal_stream
 
 def test_streakline():
+    # potential = gp.LogarithmicPotential(v_c=0.2, r_h=20., q1=1., q2=1., q3=1., units=galactic)
+    # potential = gp.MiyamotoNagaiPotential(m=1E11, a=6.5, b=0.26, units=galactic)
     potential = gp.SphericalNFWPotential(v_c=0.2, r_s=20., units=galactic)
     release_every = 1
-    time = 1024
+    time = 2048
     dt = 2
     nsteps = time // dt
 
     # w0 = np.array([0.,15.,0,-0.2,0,0]) # circular
-    w0 = np.array([0.,15.,0,-0.15,0,0]) # eccentric
+    w0 = np.array([0.,15.,0,-0.13,0,0]) # eccentric
 
     print("integrating parent orbit")
     torig,w = potential.integrate_orbit(w0, dt=-dt, nsteps=nsteps, Integrator=gi.DOPRI853Integrator)
     t,w = potential.integrate_orbit(w[-1], dt=dt, t1=torig[-1], nsteps=nsteps, Integrator=gi.DOPRI853Integrator)
     ww = w[:,0].copy()
     print("done")
+    r = np.sqrt(np.sum(ww[:,:3]**2, axis=-1))
+    ecc = (r.max() - r.min()) / (r.min() + r.max())
+    print("eccentricity", ecc)
 
-    # fig = gd.plot_orbits(w, marker=None)
+    fig = gd.plot_orbits(w, marker=None)
     # pl.show()
+    # return
 
     prog_mass = np.zeros_like(t) + 1E4
 
@@ -47,6 +53,46 @@ def test_streakline():
     pl.plot(ww[:,ixs[0]], ww[:,ixs[1]], marker=None, alpha=0.5)
     pl.scatter(stream[::2,ixs[0]], stream[::2,ixs[1]], c=t[:-1:release_every], vmin=t.min(), vmax=t.max(), cmap='coolwarm', s=4)
     pl.scatter(stream[1::2,ixs[0]], stream[1::2,ixs[1]], c=t[:-1:release_every], vmin=t.min(), vmax=t.max(), cmap='coolwarm', s=4)
-    pl.xlim(-0.6, 0.6)
-    pl.ylim(14.9,15.1)
+    # pl.xlim(-0.6, 0.6)
+    # pl.ylim(14.9,15.1)
+    pl.show()
+
+def test_fardal():
+    # potential = gp.LogarithmicPotential(v_c=0.2, r_h=20., q1=1., q2=1., q3=1., units=galactic)
+    # potential = gp.MiyamotoNagaiPotential(m=1E11, a=6.5, b=0.26, units=galactic)
+    potential = gp.SphericalNFWPotential(v_c=0.2, r_s=20., units=galactic)
+    release_every = 1
+    time = 2048
+    dt = 2
+    nsteps = time // dt
+
+    # w0 = np.array([0.,15.,0,-0.2,0,0]) # circular
+    w0 = np.array([0.,15.,0,-0.13,0,0]) # eccentric
+
+    print("integrating parent orbit")
+    torig,w = potential.integrate_orbit(w0, dt=-dt, nsteps=nsteps, Integrator=gi.DOPRI853Integrator)
+    t,w = potential.integrate_orbit(w[-1], dt=dt, t1=torig[-1], nsteps=nsteps, Integrator=gi.DOPRI853Integrator)
+    ww = w[:,0].copy()
+    print("done")
+    r = np.sqrt(np.sum(ww[:,:3]**2, axis=-1))
+    ecc = (r.max() - r.min()) / (r.min() + r.max())
+    print("eccentricity", ecc)
+
+    fig = gd.plot_orbits(w, marker=None)
+    # pl.show()
+    # return
+
+    prog_mass = np.zeros_like(t) + 1E4
+
+    stream = fardal_stream(potential.c_instance, t, ww,
+                           release_every=release_every, G=potential.G,
+                           prog_mass=prog_mass)
+
+    ixs = [0,1]
+    pl.figure(figsize=(6,6))
+    pl.plot(ww[:,ixs[0]], ww[:,ixs[1]], marker=None, alpha=0.5)
+    pl.scatter(stream[::2,ixs[0]], stream[::2,ixs[1]], c=t[:-1:release_every], vmin=t.min(), vmax=t.max(), cmap='coolwarm', s=4)
+    pl.scatter(stream[1::2,ixs[0]], stream[1::2,ixs[1]], c=t[:-1:release_every], vmin=t.min(), vmax=t.max(), cmap='coolwarm', s=4)
+    # pl.xlim(-0.6, 0.6)
+    # pl.ylim(14.9,15.1)
     pl.show()
