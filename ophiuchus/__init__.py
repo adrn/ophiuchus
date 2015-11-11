8b208d759f83e92b6bb0ab1e4a5ce4a5d1327c70
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-11 10:02:14 -0500

    added reference frame

--- ophiuchus/__init__.py
+++ ophiuchus/__init__.py
@@ -12,4 +12,13 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    pass
+    import astropy.coordinates as coord
+    import astropy.units as u
+
+    # Galactocentric reference frame to use for this project
+    galactocentric_frame = coord.Galactocentric(z_sun=0.*u.pc,
+                                                galcen_distance=8*u.kpc)
+    vcirc = 220.*u.km/u.s
+    vlsr = [-11.1, 24, 7.25]*u.km/u.s
+
+    del coord, u
