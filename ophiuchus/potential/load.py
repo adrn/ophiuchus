155c7ac1a586384e700e0d31d053c7616c982b37
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-19 10:12:06 -0500

    specify params on loading

--- ophiuchus/potential/load.py
+++ ophiuchus/potential/load.py
@@ -8,9 +8,15 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 from astropy.utils.data import get_pkg_data_filename
 import gary.potential as gp
 
-def load_potential(name):
+def load_potential(name, bar=None):
     from .. import potential as op
-    return gp.load(get_pkg_data_filename('yml/{}.yml'.format(name)), module=op)
+    pot = gp.load(get_pkg_data_filename('yml/{}.yml'.format(name)), module=op)
+    if bar is not None:
+        pars = pot.parameters
+        pars['bar']['alpha'] = bar['alpha']
+        pars['bar']['Omega'] = bar['Omega']
+        pot = op.OphiuchusPotential(**pars)
+    return pot
 
 # barred_mw = load_potential('barred_mw')
 # static_mw = load_potential('static_mw')
