ea179b3a36c4c3881971272c2e0e2b3ba08f3c03
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-11-21 11:02:00 -0500

    return 2d obj in 2d input

--- ophiuchus/util.py
+++ ophiuchus/util.py
@@ -21,4 +21,8 @@ def integrate_forward_backward(potential, w0, t_forw, t_back, dt=0.5):
                                      Integrator=gi.DOPRI853Integrator)
     t,w2 = potential.integrate_orbit(w0, dt=dt, t1=0., t2=t_forw,
                                      Integrator=gi.DOPRI853Integrator)
-    return np.vstack((w1[::-1,0], w2[1:,0]))
+    w_out = np.vstack((w1[::-1], w2[1:]))
+    if w_out.shape[1] == 1:
+        return w_out[:,0]
+    else:
+        return w_out
