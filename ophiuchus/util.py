4435c6bac23126bc496478b08e1b385dde3e6510
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-04 12:37:05 -0500

    wrote a docstring

--- ophiuchus/util.py
+++ ophiuchus/util.py
@@ -7,20 +7,39 @@ from __future__ import division, print_function
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Third-party
-import numpy as np
 import gary.integrate as gi
 from gary.dynamics.orbit import combine
 
 __all__ = ['integrate_forward_backward']
 
 def integrate_forward_backward(potential, w0, t_forw, t_back, dt=0.5,
-                               Integrator=gi.DOPRI853Integrator):
+                               Integrator=gi.DOPRI853Integrator, t0=0.):
     """
-    TODO:
+    Integrate an orbit forward and backward from a point and combine
+    into a single orbit object.
+
+    Parameters
+    ----------
+    potential : :class:`gary.potential.PotentialBase`
+    w0 : :class:`gary.dynamics.CartesianPhaseSpacePosition`, array_like
+    t_forw : numeric
+        The amount of time to integate forward in time (a positive number).
+    t_back : numeric
+        The amount of time to integate backwards in time (a negative number).
+    dt : numeric (optional)
+        The timestep.
+    Integrator : :class:`gary.integrate.Integrator` (optional)
+        The integrator class to use.
+    t0 : numeric (optional)
+        The initial time.
+
+    Returns
+    -------
+    orbit : :class:`gary.dynamics.CartesianOrbit`
     """
 
-    o1 = potential.integrate_orbit(w0, dt=-dt, t1=0., t2=t_back, Integrator=Integrator)
-    o2 = potential.integrate_orbit(w0, dt=dt, t1=0., t2=t_forw, Integrator=Integrator)
+    o1 = potential.integrate_orbit(w0, dt=-dt, t1=t0, t2=t_back, Integrator=Integrator)
+    o2 = potential.integrate_orbit(w0, dt=dt, t1=t0, t2=t_forw, Integrator=Integrator)
 
     o1 = o1[::-1]
     o2 = o2[1:]
