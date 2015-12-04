a1ef9ab2acaa64a761aee3dd14ccc14e26c1a1d2
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-04 17:53:06 -0500

    update tests

--- ophiuchus/tests/test_orbitfit.py
+++ ophiuchus/tests/test_orbitfit.py
@@ -6,10 +6,6 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-
 # Third-party
 from astropy import log as logger
 import astropy.units as u
