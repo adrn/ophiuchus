1bb2c0cfe7df716ed75c89d440a90cbe48d3f66b
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-12-09 15:14:04 -0500

    update lyapunov script

--- scripts/lyapunov.py
+++ scripts/lyapunov.py
@@ -13,35 +13,61 @@ import os
 from astropy import log as logger
 import numpy as np
 import gary.dynamics as gd
+from six.moves import cPickle as pickle
 
 # This project
+from ophiuchus import RESULTSPATH
 import ophiuchus.potential as op
 
-def main(path, overwrite=False):
-    dt = 2.
-    nsteps = 2560000 # 256 steps per orbit -- 10000 orbital periods
+def main(potential_name, results_path=None, overwrite=False):
+    # top-level output path for saving (this will create a subdir within output_path)
+    if results_path is None:
+        top_path = RESULTSPATH
+    else:
+        top_path = os.path.abspath(os.path.expanduser(results_path))
 
-    path = os.path.abspath(path)
-    name = os.path.basename(path)
-    output_file = os.path.join(path, 'lyap.npy')
-    t_output_file = os.path.join(path, 'lyap_t.npy')
-    ws_output_file = os.path.join(path, 'lyap_ws.npy')
+    if top_path is None:
+        raise ValueError("If $PROJECTSPATH is not set, you must provide a path to save "
+                         "the results in with the --results_path argument.")
 
+    output_path = os.path.join(top_path, potential_name, "lyapunov")
+    logger.debug("Output path: {}".format(output_path))
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    T = 512. # The orbits have periods ~500 Myr
+    dt = T / 512 # 512 steps per orbit
+    # nsteps = int(16384 * 512) # 16384 orbital periods
+    nsteps = int(64 * 512) # TESTING HACK HACK HACK
+
+    output_file = os.path.join(output_path, 'lyap.pickle')
     if os.path.exists(output_file) and overwrite:
         os.remove(output_file)
 
     if os.path.exists(output_file):
         logger.info("Output file exists. Exiting.")
+        return
 
     # load initial conditions file
-    w0 = np.ascontiguousarray(np.load(os.path.join(path, 'w0.npy'))[0]) # load mean orbit
-    pot = op.load_potential(name)
+    w0_path = os.path.join(top_path, potential_name, 'orbitfit', 'w0.npy')
+    w0 = np.ascontiguousarray(np.load(w0_path)[0]) # load only the mean orbit
+    pot = op.load_potential(potential_name)
 
     # compute lyapunov exponent
-    lyap,t,ws = gd.fast_lyapunov_max(w0, pot, dt=dt, nsteps=nsteps)
-    np.save(output_file, lyap)
-    np.save(t_output_file, t)
-    np.save(ws_output_file, ws)
+    lyap,orbit = gd.fast_lyapunov_max(w0, pot, dt=dt, nsteps=nsteps)
+    with open(output_file, 'wb') as f:
+        pickle.dump(lyap, f)
+    # np.save(output_file, lyap.decompose(pot.units).value)
+
+    # orbit_output_file = os.path.join(output_path, 'orbit.npy')
+    orbit_output_file = os.path.join(output_path, 'orbit.pickle')
+    with open(orbit_output_file, 'wb') as f:
+        pickle.dump(orbit, f)
+    # np.save(orbit_output_file, (orbit.t.decompose(pot.units).value,
+    #                             orbit.pos.decompose(pot.units).value,
+    #                             orbit.vel.decompose(pot.units).value))
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -56,8 +82,10 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--overwrite", dest="overwrite", default=False,
                         action="store_true", help="Overwrite any existing data.")
 
-    parser.add_argument("--path", dest="path",
-                        required=True, help="Path to the output file.")
+    parser.add_argument("--results-path", dest="results_path", default=None,
+                        help="Path to save the output file.")
+    parser.add_argument("--potential", dest="potential_name", required=True,
+                        help="Name of the potential YAML file.")
 
     args = parser.parse_args()
 
@@ -68,4 +96,4 @@ if __name__ == "__main__":
     else:
         logger.setLevel(logging.INFO)
 
-    main(args.path, overwrite=args.overwrite)
+    main(args.potential_name, results_path=args.results_path, overwrite=args.overwrite)
