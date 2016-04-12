7f7a72c2fb326adf7b82914ebbd48ce05aa51b7a
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-12 15:02:15 -0400

    anti test

--- ophiuchus/potential/tests/test_load.py
+++ ophiuchus/potential/tests/test_load.py
@@ -4,18 +4,16 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-import logging
-
 # Third-party
-from astropy import log as logger
-import matplotlib.pyplot as pl
-import numpy as np
+from astropy.tests.helper import quantity_allclose
 
 # Project
 from ..load import load_potential
 
 def test_load():
-    p = load_potential('barred_mw')
+    p_bar = load_potential('barred_mw')
+    p_static = load_potential('static_mw')
+
+    x = [4.1, -0.5, 0.1]
+    assert not quantity_allclose(p_bar.value(x), p_static.value(x))
+    assert not quantity_allclose(p_bar.gradient(x), p_static.gradient(x))
