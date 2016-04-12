33b0cada4e622ffa00f6aaebf2d579ac1d0395b2
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-12 18:06:19 -0400

    update yml files for new gary format

--- scripts/private/make-potential-ymls.py
+++ scripts/private/make-potential-ymls.py
@@ -1,3 +1,4 @@
+import sys
 import yaml
 import astropy.units as u
 from gary.units import galactic
@@ -8,14 +9,22 @@ Omegas = [40,50,60]*u.km/u.s/u.kpc
 with open("/Users/adrian/projects/ophiuchus/ophiuchus/potential/yml/barred_mw.yml") as f:
     yaml_pot = yaml.load(f)
 
+comp_ix = None
+for i,comp in enumerate(yaml_pot['components']):
+    if comp['class'] == 'WangZhaoBarPotential':
+        comp_ix = i
+
+if comp_ix is None:
+    raise ValueError("Something is horribly wrong")
+
 i = 1
 for a in alphas:
     for O in Omegas:
         alpha = a.decompose(galactic).value
         Omega = O.decompose(galactic).value
 
-        yaml_pot['parameters']['bar']['alpha'] = alpha
-        yaml_pot['parameters']['bar']['Omega'] = Omega
+        yaml_pot['components'][comp_ix]['parameters']['alpha'] = alpha
+        yaml_pot['components'][comp_ix]['parameters']['Omega'] = Omega
 
         fn = "/Users/adrian/projects/ophiuchus/ophiuchus/potential/yml/barred_mw_{}.yml".format(i)
         with open(fn, 'w') as f:
