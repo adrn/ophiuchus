b6c8a63ffa589a9d050785f6fdb31f8b3b7a3b26
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-12 14:50:40 -0400

    also test oph potential

--- ophiuchus/potential/tests/test_core.py
+++ ophiuchus/potential/tests/test_core.py
@@ -9,10 +9,16 @@ import astropy.units as u
 import numpy as np
 
 # Project
-from .. import WangZhaoBarPotential
+from .. import WangZhaoBarPotential, OphiuchusPotential
 
 def test_wangzhao():
     p = WangZhaoBarPotential(1E10, 1., 27*u.degree, 60*u.km/u.s/u.kpc)
     print(p.value([4., 0, 0]))
     print(p.gradient([4., 0, 0]))
     print(p.density([4., 0, 0]).to(u.Msun/u.pc**3))
+
+def test_oph():
+    p = OphiuchusPotential()
+    print(p.value([8., 0, 0]))
+    print(p.gradient([8., 0, 0]))
+    print(p.density([8., 0, 0]).to(u.Msun/u.pc**3))
